95a1866e59a9d78c5fa6dc071d50386e30e3151d
ejguzman3988 <eriberto.guzman@outlook.com>
2020-09-17 17:57:37 -0400

    Added rails backend

--- recipe-frontend/index.js
+++ recipe-frontend/index.js
@@ -10,14 +10,17 @@ function randomRecipe(){
     const elements = []
     const title = document.createElement('h1')
     const summary = document.createElement('p')
+    const instructions = document.createElement('div')      
     
-    elements.push(title, summary)
+    elements.push(title, summary, instructions)
 
     fetch(recipeURL + 'random?number=1&' + apiKey)
         .then(response => response.json())
         .then(data => {
             title.innerHTML = (data.recipes[0].title)
             summary.innerHTML += "<br>" + data.recipes[0].summary
+            instructions.innerHTML = "<br>" + data.recipes[0].instructions
+            console.log(data)
         });
 
     function appendDisplays(arrayOfElements){
